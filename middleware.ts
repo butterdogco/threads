55f1498ec07997d19aeff510446c494b6f0122c8
Butter Dog CEO <106115826+butterdogco@users.noreply.github.com>
2023-08-18 22:34:11 -0700

    attempt to fix something

--- middleware.ts
+++ middleware.ts
@@ -5,10 +5,10 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // An array of public routes that don't require authentication.
-  publicRoutes: ["/api/webhook/clerk"],
+  publicRoutes: ["/app/api/webhook/clerk"],
 
   // An array of routes to be ignored by the authentication middleware.
-  ignoredRoutes: ["/api/webhook/clerk"],
+  ignoredRoutes: ["/app/api/webhook/clerk"],
 });
 
 export const config = {
